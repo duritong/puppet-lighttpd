0f570f4d2185ccadf4fbc6839c6423bd75ed7476
mh <mh@immerda.ch>
2010-10-16 17:36:23 +0200

    add dependency

--- manifests/debian.pp
+++ manifests/debian.pp
@@ -1,8 +1,10 @@
 class lighttpd::debian inherits lighttpd::base {
   file{
     '/var/www/lighttpd':
-      ensure => directory;
+      ensure => directory,
+      require => Package['lighttpd'];
     '/var/www/index.lighttpd.html':
       ensure => absent,
+      require => Package['lighttpd'];
   }
 }
