2408b555be4021b02a7a853347a3224db8b09ac0
mh <mh@immerda.ch>
2010-10-16 18:05:54 +0200

    use abstract define to deploy config

--- manifests/munin.pp
+++ manifests/munin.pp
@@ -1,19 +1,16 @@
 class lighttpd::munin {
-  file{'/etc/lighttpd/conf.d/status.conf':
+  lighttpd::config::file{'status':
     content => 'server.modules += ( "mod_status" )
 $HTTP["remoteip"] == "127.0.0.1" {
   status.status-url = "/server-status"
   status.config-url = "/server-config"
 }
 ',
-    require => Package['lighttpd'],
-    notify => Service['lighttpd'],
-    owner => root, group => 0, mode => 0644;
   }
   munin::plugin::deploy{'lighttpd_': 
     source => "lighttpd/munin/lighttpd_", 
     ensure => absent,
-    require => File['/etc/lighttpd/conf.d/status.conf'],
+    require => Lighttpd::Config::File['status'],
   }
   munin::plugin{'lighttpd_total_accesses': 
     require => Munin::Plugin::Deploy['lighttpd_'],
