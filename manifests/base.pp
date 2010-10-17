ff988cdc1eac0f4aa5d15d5a8a64664b50c245fd
mh <mh@immerda.ch>
2010-10-17 13:25:27 +0200

    add cleanup cron for compressed files
    According to http://redmine.lighttpd.net/wiki/1/Docs:ModCompress the
    cleanup compress is left to the user. As we enabled compress by default
    we also install a cleanup command by default.

--- manifests/base.pp
+++ manifests/base.pp
@@ -1,30 +1,36 @@
 class lighttpd::base {
-    package{'lighttpd':
-        ensure => installed,
-    }
+  package{'lighttpd':
+    ensure => installed,
+  }
 
-    service{lighttpd:
-        ensure => running,
-        enable => true,
-        hasstatus => true, 
-        require => Package[lighttpd],
-    }
+  service{lighttpd:
+    ensure => running,
+    enable => true,
+    hasstatus => true, 
+    require => Package[lighttpd],
+  }
 
-    file{'/etc/lighttpd/lighttpd.conf':
-        source => [ "puppet:///modules/site-lighttpd/${fqdn}/lighttpd.conf",
-                    "puppet:///modules/site-lighttpd/lighttpd.conf",
-                    "puppet:///modules/lighttpd/${operatingsystem}/lighttpd.conf",
-                    "puppet:///modules/lighttpd/lighttpd.conf" ],
-        require => Package['lighttpd'],
-        notify => Service['lighttpd'],
-        owner => root, group => 0, mode => 0644;
-    }
+  file{'/etc/lighttpd/lighttpd.conf':
+    source => [ "puppet:///modules/site-lighttpd/${fqdn}/lighttpd.conf",
+                "puppet:///modules/site-lighttpd/lighttpd.conf",
+                "puppet:///modules/lighttpd/${operatingsystem}/lighttpd.conf",
+                "puppet:///modules/lighttpd/lighttpd.conf" ],
+    require => Package['lighttpd'],
+    notify => Service['lighttpd'],
+    owner => root, group => 0, mode => 0644;
+  }
 
-    # ToDo: put that in a common module to share with apache
-    file { 'default_lighttpd_index':
-        path => '/var/www/lighttpd/index.html',
-        ensure => file,
-        content => template('lighttpd/default/default_index.erb'),
-        owner => root, group => 0, mode => 0644;
-    }
+  # ToDo: put that in a common module to share with apache
+  file { 'default_lighttpd_index':
+    path => '/var/www/lighttpd/index.html',
+    ensure => file,
+    content => template('lighttpd/default/default_index.erb'),
+    owner => root, group => 0, mode => 0644;
+  }
+
+  file{'/etc/cron.daily/clean_lighttpd_compress':
+    content => "find /var/cache/lighttpd/compress -type f -mtime +10 | xargs -r rm\n",
+    require => Package['lighttpd'],
+    owner => root, group => 0, mode => 0640;
+  }
 }
