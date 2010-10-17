657cbbaee9aef1ef536d1cfa4c22d99653facd42
mh <mh@immerda.ch>
2010-10-17 15:53:58 +0200

    do the vhosts dir at the right place

--- manifests/vhosts.pp
+++ manifests/vhosts.pp
@@ -8,4 +8,10 @@ class lighttpd::vhosts {
     notify => Service['lighttpd'],
     owner => root, group => 0, mode => 0644;
   }
+
+  file{'/var/www/vhosts':
+    ensure => directory,
+    require => Package['lighttpd'],
+    owner => root, group => 0, mode => 0644;
+  }
 }
