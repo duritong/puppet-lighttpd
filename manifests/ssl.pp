da21bf7ba76203d693fe01be4ef49b9b5fef6d87
mh <mh@immerda.ch>
2012-06-18 12:59:26 -0300

    configure things properly

--- manifests/ssl.pp
+++ manifests/ssl.pp
@@ -1,8 +1,10 @@
 class lighttpd::ssl(
-  $manage_shorewall = false
+  $manage_shorewall = false,
+  $manage_munin = false
 ) {
   class{'lighttpd':
-    manage_shorewall => $manage_shorewall
+    manage_shorewall => $manage_shorewall,
+    manage_munin => $manage_munin,
   }
   lighttpd::config::file{ 'ssl': }
   if $manage_shorewall {
