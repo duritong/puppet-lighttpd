324dc2346b0357fb9b5dd55b68603b22fc0129d5
mh <mh@immerda.ch>
2014-07-02 21:11:09 +0200

    also pass down the cluster_node parameter

--- manifests/ssl.pp
+++ manifests/ssl.pp
@@ -1,10 +1,13 @@
+# ssl enabled lighttpd
 class lighttpd::ssl(
+  $cluster_node     = $cluster_node,
   $manage_shorewall = false,
-  $manage_munin = false
+  $manage_munin     = false,
 ) {
   class{'lighttpd':
+    cluster_node     => $cluster_node,
     manage_shorewall => $manage_shorewall,
-    manage_munin => $manage_munin,
+    manage_munin     => $manage_munin,
   }
   lighttpd::config::file{ 'ssl': }
   if $manage_shorewall {
