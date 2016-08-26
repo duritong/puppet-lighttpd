7ec339b003ce80459d9b17b9f1c904976cf03bef
mh <mh@immerda.ch>
2016-08-26 14:57:16 +0200

    update to the latest epel release, status.conf is now included in the rpm

--- manifests/munin.pp
+++ manifests/munin.pp
@@ -1,31 +1,12 @@
+# setup munin plugins for lighttpd
 class lighttpd::munin {
-  lighttpd::config::file{'status':
-    content => 'server.modules += ( "mod_status" )
-$HTTP["remoteip"] =~ "^(::ffff:)?127.0.0.1$" {
-  status.status-url = "/server-status"
-  status.config-url = "/server-config"
-}
-',
-  }
-  munin::plugin::deploy{'lighttpd_': 
-    source => "lighttpd/munin/lighttpd_", 
-    ensure => absent,
+  munin::plugin::deploy{'lighttpd_':
+    ensure  => absent, # this is not a direct plugin
+    source  => 'lighttpd/munin/lighttpd_',
     require => Lighttpd::Config::File['status'],
-  }
-  munin::plugin{'lighttpd_total_accesses': 
-    require => Munin::Plugin::Deploy['lighttpd_'],
-    ensure => 'lighttpd_',
-  }
-  munin::plugin{'lighttpd_total_kbytes': 
-    require => Munin::Plugin::Deploy['lighttpd_'],
-    ensure => 'lighttpd_',
-  }
-  munin::plugin{'lighttpd_uptime': 
-    require => Munin::Plugin::Deploy['lighttpd_'],
-    ensure => 'lighttpd_',
-  }
-  munin::plugin{'lighttpd_busyservers': 
-    require => Munin::Plugin::Deploy['lighttpd_'],
-    ensure => 'lighttpd_',
+  } -> munin::plugin{
+    [ 'lighttpd_total_accesses', 'lighttpd_total_kbytes', 'lighttpd_uptime',
+      'lighttpd_busyservers', ]:
+    ensure => 'lighttpd_';
   }
 }
