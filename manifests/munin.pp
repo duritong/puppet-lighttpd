33fa21e57a705aaa2db10e07e6abeee1a836eb53
mh <mh@immerda.ch>
2016-08-26 15:05:21 +0200

    this dependency is gone

--- manifests/munin.pp
+++ manifests/munin.pp
@@ -3,7 +3,6 @@ class lighttpd::munin {
   munin::plugin::deploy{'lighttpd_':
     ensure  => absent, # this is not a direct plugin
     source  => 'lighttpd/munin/lighttpd_',
-    require => Lighttpd::Config::File['status'],
   } -> munin::plugin{
     [ 'lighttpd_total_accesses', 'lighttpd_total_kbytes', 'lighttpd_uptime',
       'lighttpd_busyservers', ]:
