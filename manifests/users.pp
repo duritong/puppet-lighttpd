c7a6088d10128bc719ee181eb0e2fe2e5dfb84c7
mh <mh@immerda.ch>
2017-02-24 12:13:54 +0100

    enable config on redhat based systems

--- manifests/users.pp
+++ manifests/users.pp
@@ -1,6 +1,13 @@
 # manage a users resource
 class lighttpd::users {
   include ::lighttpd
+  if $osfamily == 'RedHat' {
+    file_line{'userdir':
+      path   => "${lighttpd::conf_dir}/config.conf",
+      line   => "include \"${lighttpd::conf_dir_name}/userdir.conf\"",
+      notify => Service['lighttpd'],
+    }
+  }
   file{'/var/www/users':
     ensure  => directory,
     require => Package['lighttpd'],
